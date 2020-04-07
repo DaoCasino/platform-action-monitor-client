3b77adacd8388bb080b751cc08cbf8df872e3b6e
Gar|k <garik.djan@gmail.com>
2020-04-07 20:28:18 +0300

    feat: change default message size to 10K

--- listener.go
+++ listener.go
@@ -13,7 +13,7 @@ const (
 	writeWait      = 10 * time.Second
 	pongWait       = 60 * time.Second
 	pingPeriod     = (pongWait * 9) / 10
-	maxMessageSize = 1024 * 4
+	maxMessageSize = 1024 * 10
 )
 
 type EventListener struct {
