c62f75920b18012564e7300d1375b733f3a5947a
Gar|k <garik.djan@gmail.com>
2020-04-09 19:04:08 +0300

    feat: change data to json raw message

--- event.go
+++ event.go
@@ -1,19 +1,20 @@
 package eventlistener
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
 type EventType int
 
 type Event struct {
-	Offset    uint64      `json:"offset"`
-	Sender    string      `json:"sender"`
-	CasinoID  string      `json:"casino_id"`
-	GameID    string      `json:"game_id"`
-	RequestID string      `json:"req_id"`
-	EventType EventType   `json:"event_type"`
-	Data      interface{} `json:"data"` // TODO: ??? interface or json raw message ?
+	Offset    uint64          `json:"offset"`
+	Sender    string          `json:"sender"`
+	CasinoID  string          `json:"casino_id"`
+	GameID    string          `json:"game_id"`
+	RequestID string          `json:"req_id"`
+	EventType EventType       `json:"event_type"`
+	Data      json.RawMessage `json:"data"` // TODO: ??? interface or json raw message ?
 }
 
 type EventMessage struct {
